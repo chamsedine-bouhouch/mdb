7fc98d96dd1c0dd9ca773aa64d252d5feb7ec133
chamseddine bouhouch <bouhouchchamseddine@gmail.com>
2020-03-17 20:56:35 +0100

    second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,8 @@
   <!-- Google Fonts Roboto -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
   <!-- Bootstrap core CSS -->
-<link rel="stylesheet" href="{{asset('asset/css/bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{asset('asset/css/bootstrap.min.css')}}">
+  <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
   <!-- Material Design Bootstrap -->
   <link rel="stylesheet" href="{{asset('asset/css/mdb.min.css')}}">
   <!-- Your custom styles (optional) -->
@@ -20,7 +21,10 @@
   <link rel="stylesheet" type="text/css" href="{{asset('fonts/flaticon/font/flaticon.css')}}">
     <link rel="stylesheet" href="{{asset('css/flaticon.css')}}">
 
+
   <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+   
+
    
 
 </head>
@@ -113,12 +117,8 @@ select {
             <a class="dropdown-item" href="#">Another action</a>
             <a class="dropdown-item" href="#">Something else here</a>
           </div>
-
-          
-          <li class="nav-item">
-         
         </li>
-        </li>
+        
       </ul>
 <form class="form-inline">
         <div class="md-form my-0">
@@ -154,7 +154,7 @@ select {
 
 
  <!-- SERVICE -->
-      <div class="container my-5">
+      <div class="container my-5 p-5 z-depth-1">
 
   <!-- Section -->
   <section>
@@ -520,11 +520,65 @@ select {
 
 </div>
 
+<div class="container my-5 p-5 z-depth-1">
+
+
+  <!--Section: Content-->
+  <section class="">
+    
+    <h2 class="font-weight-bold mb-4 pb-2 text-center ">Counter
+</h2>
+    <hr class="w-header my-4" style="background-color:  #fe4a49">
+
+    <div class="row d-flex justify-content-center">
+
+      <div class="col-md-6 col-lg-3 mb-4 text-center">
+        <h4 class="h1 font-weight-normal mb-3">
+          <i class="fas fa-file-alt indigo-text"></i>
+          <span class="d-inline-block count-up" data-from="0" data-to="100" data-time="2000">100</span>
+        </h4>
+        <p class="font-weight-normal text-muted">Unique Page</p>
+      </div>
+
+      <div class="col-md-6 col-lg-3 mb-4 text-center">
+        <h4 class="h1 font-weight-normal mb-3">
+          <i class="fas fa-layer-group indigo-text"></i>
+          <span class="d-inline-block count1" data-from="0" data-to="250" data-time="2000">250</span>
+        </h4>
+        <p class="font-weight-normal text-muted">Block Variety</p>
+      </div>
+
+      <div class="col-md-6 col-lg-3 mb-4 text-center">
+        <h4 class="h1 font-weight-normal mb-3">
+          <i class="fas fa-pencil-ruler indigo-text"></i>
+          <span class="d-inline-block count2" data-from="0" data-to="330" data-time="2000">330</span>
+        </h4>
+        <p class="font-weight-normal text-muted">Reusable Component</p>
+      </div>
+      
+      <div class="col-md-6 col-lg-3 mb-4 text-center">
+        <h4 class="h1 font-weight-normal mb-3">
+          <i class="fab fa-react indigo-text"></i>
+          <span class="d-inline-block count3" data-from="0" data-to="430" data-time="2000">430</span>
+        </h4>
+        <p class="font-weight-normal text-muted">Crafted Element</p>
+      </div>
+
+    </div>
+
+  </section>
+  <!--Section: Content-->
+
+
+</div>
+
+<!-- témoi -->
+
   
 
 
 
-  <footer class="page-footer font-small mdb-color pt-4 unique-color-dark">
+  <footer class="page-footer font-small r pt-4"  style="background-color: #001730">
 
   <!-- Footer Links -->
   <div class="container text-center text-md-left">
